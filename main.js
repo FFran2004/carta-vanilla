9aed130602c9719448b1405a928003d7c4b88bef
FFran2004 <freyrefrancisc03@gmail.com>
2023-05-29 19:01:06 -0300

    agrego y comento codigo

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const clickMenu = () => {
     menu.classList.toggle('active');
   }
 
-// condicion que al tocar en cualquier parte de la pantalla se cierre el menu
+// condicion que al tocar en cualquier parte de la pantalla se cierre el menu.
   document.addEventListener('click', (event) => {
       const toggler = document.querySelector('.burguer');
       const menu = document.querySelector('.navbar-links');
@@ -15,3 +15,26 @@ const clickMenu = () => {
     toggler.classList.remove('rotate');
     }
   });
+
+
+  // funcion para boton scroll hacia el inicio.
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    var scrollToTopBtn = document.querySelector(".scroll-to-top");
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      scrollToTopBtn.classList.add("show");
+    } else {
+      scrollToTopBtn.classList.remove("show");
+    }
+  }
+
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }

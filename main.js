d9915b34bcb75259e0c989270e6ea77b7f43c750
FFran2004 <freyrefrancisc03@gmail.com>
2023-06-27 17:19:50 -0300

    modifico code 707

--- main.js
+++ main.js
@@ -1,40 +1,13 @@
-const clickMenu = () => {
-    const toggler = document.querySelector('.burguer');
-    const menu = document.querySelector('.navbar-links');
-    toggler.classList.toggle('rotate');
-    menu.classList.toggle('active');
-  }
+handleMenuIconClick = () => {
+  document.body.classList.toggle("open");
+}
 
-// condicion que al tocar en cualquier parte de la pantalla se cierre el menu.
-  document.addEventListener('click', (event) => {
-      const toggler = document.querySelector('.burguer');
-      const menu = document.querySelector('.navbar-links');
-  
-    if (!menu.contains(event.target) && !toggler.contains(event.target)) {
-    menu.classList.remove('active');
-    toggler.classList.remove('rotate');
-    }
-  });
+hideMenu = () => {
+  document.body.classList.remove("open");
+}
 
+// funcion para boton scroll hacia el inicio.
+function redirectInicio() {
+  window.location.href = "#";
+}
 
-  // funcion para boton scroll hacia el inicio.
-  window.onscroll = function () {
-    scrollFunction();
-  };
-
-  function scrollFunction() {
-    var scrollToTopBtn = document.querySelector(".scroll-to-top");
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      scrollToTopBtn.classList.add("show");
-    } else {
-      scrollToTopBtn.classList.remove("show");
-    }
-  }
-
-  function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
